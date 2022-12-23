f5c15ab469c7662761f7acba520e0bde11ab0c80
kalanchoe18 <marcoaleman18@gmail.com>
2022-12-23 18:18:19 -0300

    ensajes

--- src/components/market/ProductsCtn.vue
+++ src/components/market/ProductsCtn.vue
@@ -7,18 +7,44 @@ import { getProducts } from '../../firebase/products';
 
 
 const cart = ref([]);
+const msjCarrito = ref({
+    clases: {
+        display: 'd-none',
+        color: 'danger'
+    },
+    msj: ''
+})
 
 const addItem = (product) => {
     //if (!cart.value.includes(product.product)) {
     if (!cart.value.some(element => element.name == product.product.name)) {
         cart.value.push(product.product)
         cart.value[cart.value.length - 1].unit = 1
-        //console.log(cart.value[0].name)
-        //console.log(cart.value.length.unit);
+
+        msjCarrito.value.clases.display = 'd-flex';
+        msjCarrito.value.clases.color = 'alert-success';
+        msjCarrito.value.msj = 'Se agrego el producto al carrito.';
+        setTimeout(() => {
+            msjCarrito.value.clases.display = 'd-none';
+        }, 1500);
+
     } else {
         cart.value.map(prod => {
             if (prod.name == product.product.name/*  && prod.unit < product.product.stock */) {
                 prod.unit += 1;
+                msjCarrito.value.clases.display = 'd-flex';
+                msjCarrito.value.clases.color = 'alert-success';
+                msjCarrito.value.msj = 'Se ha sumado una unidad al carrito.';
+                setTimeout(() => {
+                    msjCarrito.value.clases.display = 'd-none';
+                }, 1500);
+            } else {
+                /* msjCarrito.value.clases.display = 'd-flex';
+                msjCarrito.value.clases.color = 'alert-danger';
+                msjCarrito.value.msj = 'No hay mas unidades en stock.';
+                setTimeout(() => {
+                    msjCarrito.value.clases.display = 'd-none';
+                }, 1500); */
             }
         })
 
@@ -56,4 +82,14 @@ onMounted(() => {
         <Card @addItem="addItem" v-for="product in products" :product="product" />
     </div>
     <CartComponent :cart="cart" @deleteItem="deleteItem" @deleteCart="deleteCart" />
+
+    <div :class="[msjCarrito.clases.display, msjCarrito.clases.color]"
+        class="alert alert-success d-flex align-items-center justify-content-center w-50 fixed-bottom text-center " role="alert"
+        id="mensaje-carrito" v-text="msjCarrito.msj"></div>
 </template>
+
+<style scoped>
+#mensaje-carrito {
+    margin: 0 auto 10px;
+}
+</style>

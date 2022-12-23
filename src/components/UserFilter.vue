644d1031ce37d252870def2ffa2981f5f33e2341
Lautaro Elias <lautaro100798programacion@gmail.com>
2022-12-23 00:31:12 -0300

    prueba

---
+++ src/components/UserFilter.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref, computed, onMounted } from 'vue'
+import CommentPost from '../components/CommentPost.vue'
+import comments from '../store/commentStore.js'
+
+const commentsWithId = ref([])
+
+const props = defineProps({
+    postId: "",
+    typeOf: String,
+})
+
+const waitComment = computed(() => {
+    commentsWithId.value = comments.value.filter(comment => comment.postId === props.postId)
+})
+
+
+</script>
+<template>
+    {{ waitComment }}
+    <CommentPost v-for="comment in commentsWithId" :comment="comment" :key="comment.id" />
+</template> 
